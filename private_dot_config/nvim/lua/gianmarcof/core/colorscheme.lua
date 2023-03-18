2d6e77ed53a0bed7c40bfa572b8c716320a3941d
Gianmarco Folchi <gmfolc@gmail.com>
2023-03-18 00:01:30 -0400

    Update .config/nvim/lua/gianmarcof/core/colorscheme.lua Update .config/nvim/lua/gianmarcof/plugins-setup.lua

--- private_dot_config/nvim/lua/gianmarcof/core/colorscheme.lua
+++ private_dot_config/nvim/lua/gianmarcof/core/colorscheme.lua
@@ -1,7 +1,7 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme oxocarbon")
+local status, _ = pcall(vim.cmd, "colorscheme nightfly")
 if not status then
-	print("Colorscheme not found! (Probably need to edit packer_compiled.lua)") -- print error if colorscheme not installed
+	print("Colorscheme not found! (Probably need to edit colorscheme.lua)") -- print error if colorscheme not installed
 	return
 end
