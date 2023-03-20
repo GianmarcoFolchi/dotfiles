56cd6088361c482fa53e3ee4d2083c0bab036917
Gianmarco Folchi <gmfolc@gmail.com>
2023-03-20 19:10:48 -0400

    Update .config/nvim/lua/gianmarcof/plugins/buffer_manager.lua

--- private_dot_config/nvim/lua/gianmarcof/plugins/buffer_manager.lua
+++ private_dot_config/nvim/lua/gianmarcof/plugins/buffer_manager.lua
@@ -1,15 +1,31 @@
 -- import comment plugin safely
 local setup, buffer_manager = pcall(require, "buffer_manager")
 if not setup then
-  return
+	return
 end
+-- import telescope actions safely
+local actions_setup, bmui = pcall(require, "buffer_manager.ui")
+if not actions_setup then
+	return
+end
+
 local map = vim.keymap.set
-local opts = {noremap = true}
+local opts = { noremap = true }
+
 -- enable buffer manager
 buffer_manager.setup({
-  local bmui = require("buffer_manager.ui")
 
-  map({ 't', 'n' }, '<leader>b', bmui.toggle_quick_menu, opts)
-  map('n', '<C-j>', bmui.nav_next, opts)
-  map('n', '<C-k>', bmui.nav_prev, opts) 
+	defaults = {
+		mappings = {
+			i = {
+				["<leader>b"] = bmui.toggle_quick_menu,
+				["<C-j>"] = bmui.nav_next,
+				["<C-k>"] = bmui.nav_prev,
+			},
+		},
+	},
+
+	-- map({ 't', 'n' }, '<leader>b', bmui.toggle_quick_menu, opts)
+	-- map('n', '<C-j>', bmui.nav_next, opts)
+	-- map('n', '<C-k>', bmui.nav_prev, opts)
 })
