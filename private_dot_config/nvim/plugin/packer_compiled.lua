c209e2f7ba3256ee83f3b3335f7accca5c1af000
Gianmarco Folchi <gmfolc@gmail.com>
2023-03-20 13:05:57 -0400

    Update .config/nvim/plugin/packer_compiled.lua

--- private_dot_config/nvim/plugin/packer_compiled.lua
+++ private_dot_config/nvim/plugin/packer_compiled.lua
@@ -115,7 +115,7 @@ _G.packer_plugins = {
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["dashboard-nvim"] = {
-    config = { "\27LJ\2\nJ\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\ntheme\nhyper\nsetup\14dashboard\frequire\0" },
+    config = { "\27LJ\2\nR\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\venable\2\ntheme\nhyper\nsetup\14dashboard\frequire\0" },
     loaded = false,
     needs_bufread = false,
     only_cond = false,
