3bb0af580179551875e58aae328906513abf1a07
Gianmarco Folchi <gmfolc@gmail.com>
2023-03-20 18:43:23 -0400

    Update .config/nvim/lua/gianmarcof/core/keymaps.lua

--- private_dot_config/nvim/lua/gianmarcof/core/keymaps.lua
+++ private_dot_config/nvim/lua/gianmarcof/core/keymaps.lua
@@ -44,8 +44,10 @@ keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+--buffer-manager
+keymap.set("n", "<leader>fb", "<cmd:lua require('buffer_manager.ui').toggle_quick_menu()<cr>")
 
 -- telescope git commands (not on youtube nvim video)
 --keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
